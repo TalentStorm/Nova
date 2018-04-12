638e7355d4b283647bac8988bec24b1be4c53c00
DreamYao <805229888@qq.com>
2018-04-12 21:15:07 +0800

    结构添加

---
+++ src/main/java/com/talent/storm/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.talent.storm.controller;
+
+/**
+ * @author dreamyao
+ * @title
+ * @date 2018/4/12 下午9:12
+ * @since 1.0.0
+ */
+public class TestController {
+}
---
+++ src/main/java/com/talent/storm/dao/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author dreamyao
+ * @title
+ * @date 2018/4/12 下午9:13
+ * @since 1.0.0
+ */
+package com.talent.storm.dao;
